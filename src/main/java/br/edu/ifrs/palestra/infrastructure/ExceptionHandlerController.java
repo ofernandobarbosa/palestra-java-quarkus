77ae78c1ab4a69afea19f8b20c78b0d19613ffea
Fernando Barbosa <ofernandobarbosa@gmail.com>
2023-10-02 16:26:45 -0300

    corrigindo exception handler para PanacheRepository

--- src/main/java/br/edu/ifrs/palestra/infrastructure/ExceptionHandlerController.java
+++ src/main/java/br/edu/ifrs/palestra/infrastructure/ExceptionHandlerController.java
@@ -4,6 +4,8 @@ import br.edu.ifrs.palestra.dto.ExceptionDTO;
 import br.edu.ifrs.palestra.service.ServiceException;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,11 @@ import org.springframework.http.ResponseEntity;
 @RestControllerAdvice
 public class ExceptionHandlerController {
     
-    // @ExceptionHandler(DataIntegrityViolationException.class)
-    // public ResponseEntity<ExceptionDTO> threatDuplicateEntry(DataIntegrityViolationException exception) {
-    //     ExceptionDTO exceptionDto = new ExceptionDTO("Valor já inserido no banco de dados.", "400");
-    //     return ResponseEntity.badRequest().body(exceptionDto);
-    // }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionDTO> threatDuplicateEntry(ConstraintViolationException exception) {
+        ExceptionDTO exceptionDto = new ExceptionDTO("Valor já inserido no banco de dados.", "400");
+        return ResponseEntity.badRequest().body(exceptionDto);
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> threatNotFound404(EntityNotFoundException exception) {
